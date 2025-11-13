baf098bf50a05b1d2a377183186d0c3785614b0d
Harry2k21 <parker2002hp@gmail.com>
2025-11-13 16:23:36 +0000

    taking off certain punctuation

---
+++ app.test.js
@@ -0,0 +1,13 @@
+const isEven = require('./isEven');
+
+test('returns true for even number', () => {
+  expect(isEven(4)).toBe(true);
+});
+
+test('returns false for odd number', () => {
+  expect(isEven(5)).toBe(false);
+});
+
+test('throws error if input not a number', () => {
+  expect(() => isEven('a')).toThrow('Not a number');
+});
